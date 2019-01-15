130766fad65e3ace6038e89edbeb3cf51af76448
Michael Nissen <m9ssen@gmail.com>
2019-01-15 15:22:01 +0100

    Set up initialisation of GPS data and corbonIntensity data

--- App.js
+++ App.js
@@ -1,49 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	ActivityIndicator,
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { getLiveCarbonIntensity } from './src/utils/api';
+import { getLocationData } from './src/utils/functions';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends React.PureComponent {
+	constructor(props) {
+		super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+		this.state = {
+			color: '#FFFFFF',
+			coords: null,
+			carbonIntensityData: null,
+		}
+
+		// The update interval for the timer
+		this.UPDATE_INTERVAL = 5000;
+	}
+
+	componentDidMount() {
+		this.init();
+	}
+
+	init = () => {
+		// Initialise the location data
+		getLocationData().then((position) => {
+			this.setState({
+				coords: {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				}
+			}, () => {
+				// Initialise the carbon intensity
+				this.setCarbonIntensity(this.state.coords);
+
+				// Update the carbon intensity after UDATE_INTERVAL ms
+				setInterval(this.setCarbonIntensity, this.UPDATE_INTERVAL);
+			});
+		});
+	}
+
+	setCarbonIntensity = () => {
+		getLiveCarbonIntensity(this.state.coords).then((result) => {
+			if (result.error) {
+				return (null);
+			}
+
+			this.setState({
+				carbonIntensityData: {
+					zone: result.zone,
+					carbonIntensity: result.carbonIntensity,
+				},
+			});
+		});
+	}
+
+	renderMainView = () => {
+		const { coords } = this.state;
+		if (coords) {
+			return (
+				<View>
+					<Text>HI</Text>
+				</View>
+			);
+		}
+
+		return (
+			<ActivityIndicator size={"large"} />
+		);
+	}
+
+	render() {
+		// console.log(process.env);
+		console.log(this.state);
+		const { color } = this.state;
+		return (
+			<View style={styles.container}>
+				{this.renderMainView()}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#FFFFFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+
+const APIURL = 'https://api.electricitymap.org/v3';
+const API_TOKEN = 'rILfhiFrZ3emXcVMGU62';
+// const API_TOKEN = process.env.API_TOKEN;
+
+export function getLiveCarbonIntensity(coords) {
+    return fetch(`${APIURL}/carbon-intensity/latest?lon=${coords.lng}&lat=${coords.lat}`, {
+        method: 'GET',
+        headers: {
+            'auth-token': API_TOKEN,
+        }
+    }).then(async (response) => {
+        const json = await response.json();
+        return (json);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
